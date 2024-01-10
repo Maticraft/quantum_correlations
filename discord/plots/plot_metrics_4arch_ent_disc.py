6fe3458f1c53e36ab4f384d18f31a036662a7eef
Maticraft <mati18a@poczta.fm>
2024-01-11 00:07:03 +0100

    plot adjustments

--- discord/plots/plot_metrics_4arch_ent_disc.py
+++ discord/plots/plot_metrics_4arch_ent_disc.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 from matplotlib.transforms import offset_copy
 import numpy as np
 
-data1 = np.loadtxt('./results/discord/trace_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data2 = np.loadtxt('./results/discord/sep_pure_sep_param_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data3 = np.loadtxt('./results/discord/sep_prod_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data4 = np.loadtxt('./results/discord/sep_prod_fc4_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data5 = np.loadtxt('./results/discord/sep_zd_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data6 = np.loadtxt('./results/discord/sep_zd_fc4prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data7 = np.loadtxt('./results/discord/sep_all_sep_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data8 = np.loadtxt('./results/discord/sep_all_sep_fc4_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data9 = np.loadtxt('./results/discord/sep_nps_prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
-data10 = np.loadtxt('./results/discord/sep_nps_fc4prediction_thresh_mixed_bal_bal_acc_log.txt', delimiter='  ', skiprows=1)
+data1 = np.loadtxt('./results/3qbits/discord/trace_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data2 = np.loadtxt('./results/3qbits/discord/sep_pure_sep_param_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data3 = np.loadtxt('./results/3qbits/discord/sep_prod_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data4 = np.loadtxt('./results/3qbits/discord/sep_prod_fc4_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data5 = np.loadtxt('./results/3qbits/discord/sep_zd_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data6 = np.loadtxt('./results/3qbits/discord/sep_zd_fc4prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data7 = np.loadtxt('./results/3qbits/discord/sep_all_sep_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data8 = np.loadtxt('./results/3qbits/discord/sep_all_sep_fc4_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data9 = np.loadtxt('./results/3qbits/discord/sep_nps_prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
+data10 = np.loadtxt('./results/3qbits/discord/sep_nps_fc4prediction_thresh_mixed_bal_bal_acc_log.txt', skiprows=1)
 
 
 data_list = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10]
@@ -54,10 +54,10 @@ for i in range(0, len(data_list) - 1):
     axs[i, 0].grid(True)
     axs[i, 0].set_ylabel("Precision")
     axs[i, 0].set_xlabel("Recall")
-    axs[i, 0].plot(data1[:, 3], data1[:, 2], 'o--', markersize = 3, linewidth = 1, label='Trace reconstruction of zero-discord states')
-    axs[i, 0].plot(data1[:, 6], data1[:, 5], 'x--', markersize = 4, linewidth =1,  label='Trace reconstruction of separable states')
-    axs[i, 0].plot(data2[:, 3], data2[:, 2], 'o--', markersize = 3, linewidth = 1, label='Separator reconstruction of zero-discord states')
-    axs[i, 0].plot(data2[:, 6], data2[:, 5], 'x--', markersize = 4, linewidth =1,  label='Separator reconstruction of separable states')
+    axs[i, 0].plot(data1[:, 3], data1[:, 2], 'o-', markersize = 2, markevery=2, linewidth = 1, label='Trace reconstruction of zero-discord states')
+    axs[i, 0].plot(data1[:, 6], data1[:, 5], '-', markersize = 4, linewidth =1,  label='Trace reconstruction of separable states')
+    axs[i, 0].plot(data2[:, 3], data2[:, 2], 'o--', markersize = 2, markevery=2, linewidth = 1, label='Separator reconstruction of zero-discord states')
+    axs[i, 0].plot(data2[:, 6], data2[:, 5], '--', markersize = 4, linewidth =1,  label='Separator reconstruction of separable states')
 
     axs[i, 1].grid(True)
     axs[i, 1].set_xscale('log')
@@ -65,10 +65,10 @@ for i in range(0, len(data_list) - 1):
     #axs[i, 1].set_yticks(np.arange(0, 1.1, 0.5))
     axs[i, 1].set_ylabel("Accuracy")
     axs[i, 1].set_xlabel("Threshold")
-    axs[i, 1].plot(data1[:, 0], data1[:, 7], 'o--', markersize = 3, linewidth = 1, label='Trace reconstruction of zero-discord states')
-    axs[i, 1].plot(data1[:, 0], data1[:, 8], 'x--', markersize = 4, linewidth =1,  label='Trace reconstruction of separable states')
-    axs[i, 1].plot(data2[:, 0], data2[:, 7], 'o--', markersize = 3, linewidth = 1, label='Separator reconstruction of zero-discord states')
-    axs[i, 1].plot(data2[:, 0], data2[:, 8], 'x--', markersize = 4, linewidth =1,  label='Separator reconstruction of separable states')
+    axs[i, 1].plot(data1[:, 0], data1[:, 7], 'o-', markersize = 2, linewidth = 1, markevery=5, label='Trace reconstruction of zero-discord states')
+    axs[i, 1].plot(data1[:, 0], data1[:, 8], '-', markersize = 4, linewidth =1,  label='Trace reconstruction of separable states')
+    axs[i, 1].plot(data2[:, 0], data2[:, 7], 'o--', markersize = 2, linewidth = 1, markevery=5, label='Separator reconstruction of zero-discord states')
+    axs[i, 1].plot(data2[:, 0], data2[:, 8], '--', markersize = 4, linewidth =1,  label='Separator reconstruction of separable states')
 
 
 pad = 5
@@ -87,5 +87,5 @@ for ax, row in zip(axs[:,0], rows):
 lbls = axs[1, 0].get_legend_handles_labels()
 lgd = fig.legend(*lbls, bbox_to_anchor = (0.95, 0.01), fontsize = 14)
 plt.tight_layout(pad = 0.7, rect=[0.1, 0, 1.2, 1])
-plt.savefig('./plots/discord_metrics_final/arch_metrics.png', bbox_extra_artists=[lgd] + anns, bbox_inches='tight', format='png')
+plt.savefig('./plots/discord_arch_metrics.png', bbox_extra_artists=[lgd] + anns, bbox_inches='tight', format='png')
 plt.close()
